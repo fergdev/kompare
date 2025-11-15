7948a24a6c02de6bdb16ea98233e78e01fb04659
fergdev <fergus.a.hewson@outlook.com>
2025-11-15 19:20:38 +1300

    Adding MainViewController.kt

--- sample-kmp/src/iosMain/kotlin/io/fergdev/kompare/example/MainViewController.kt
+++ sample-kmp/src/iosMain/kotlin/io/fergdev/kompare/example/MainViewController.kt
@@ -2,4 +2,5 @@ package io.fergdev.kompare.example
 
 import androidx.compose.ui.window.ComposeUIViewController
 
+@Suppress("unused", "FunctionName")
 fun MainViewController() = ComposeUIViewController { App() }
